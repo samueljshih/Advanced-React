935253f41cd61a2b0d325beaa474c316d8791835
Samuel Shih <samuels.shih@gmail.com>
2018-11-28 11:32:07 -0800

    Working on nav bar

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,9 @@
+import React, { Component, createFactory } from 'react';
+
+const Home = props => (
+  <div>
+    <p>Hey!!!</p>
+  </div>
+);
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import React, { Component, createFactory } from 'react';
+
+const Sell = props => (
+  <div>
+    <p>Sell!!!</p>
+  </div>
+);
+
+export default Sell;
